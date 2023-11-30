0c0fb5c7c2ee44d6506db74b4f5549e0d03a234e
ren <alinaswroking@gmail.com>
2023-11-30 17:27:38 +0300

    feat: add results at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+interface SearchResult {
+  name: string;
+  description: string;
+}
+
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  
   useEffect(() => {
     // Проверка наличия ранее сохраненного поискового запроса в локальном хранилище
     const savedSearchTerm = localStorage.getItem('searchTerm');
@@ -14,6 +21,13 @@ function App() {
   const handleSearch = () => {
     localStorage.setItem('searchTerm', searchTerm); // Сохранение поискового запроса в локальном хранилище
     console.log(`Searching for: ${searchTerm}`);
+
+
+    const dummyResults: SearchResult[] = [
+      { name: 'Result', description: `${searchTerm}` },
+    ];
+
+    setSearchResults(dummyResults);
   };
 
   return (
@@ -34,7 +48,13 @@ function App() {
 
       {/* Bottom Section */}
       <div className="bottom-section">
-        <h1>Bottom Section</h1>
+        <h1>Search Results</h1>
+        {searchResults.map((result, index) => (
+          <div key={index}>
+            <h3>{result.name}</h3>
+            <p>{result.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
