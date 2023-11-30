d6906e875b9cf0184dbab800a4bdd9523721ffc8
ren <alinaswroking@gmail.com>
2023-11-30 16:40:25 +0300

    add files

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Top Section */}
+      <div className="top-section">
+        <h1>Top Section</h1>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="bottom-section">
+        <h1>Bottom Section</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
